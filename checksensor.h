5e6466cc0cbbffa4d32097bfeeabe410284d8767
Ramsucp <40172024+Ramsucp@users.noreply.github.com>
2018-06-28 18:49:24 +0200

    Create checksensor.h

---
+++ checksensor.h
@@ -0,0 +1,47 @@
+int checkSensor(){
+  bool first;
+  bool second;
+  bool directio;
+  unsigned long time_e;
+    if ( 8< *us1< 80) {
+     directio = 1;
+     first=1;
+     return first;
+     second=0;
+     return second;
+     return directio;
+     timeus2=millis();
+     return time_e;
+    }
+        
+    }
+    if ( 8< *us2< 80) {
+     
+        direction = 0;
+        first=0;
+        return first;
+        second=1;
+        return second;
+        return directio;
+        time_e=millis();
+        return time_e;
+    
+    }
+
+
+    int checkOppo() {
+     
+      while( millis()-time_e < TIME_MAX ) {
+      checksensor();
+      if( direction  && first && !second)
+      {people_in++;
+      returne people_in;
+        }
+
+         if( !direction  && !first && second)
+      {people_out--;
+      return people_out;
+        }
+                                          }
+                                          
+      } 
